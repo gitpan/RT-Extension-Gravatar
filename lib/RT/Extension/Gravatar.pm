978f0e1cdec5b769b8d180b1180e1f238aa7bf0c
Christian Loos <cloos@cpan.org>
2013-10-28 07:12:26 +0000

    Import of CLOOS/RT-Extension-Gravatar-1.00 from CPAN.
    gitpan-cpan-distribution: RT-Extension-Gravatar
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         CLOOS/RT-Extension-Gravatar-1.00.tar.gz
    gitpan-cpan-author:       CLOOS
    gitpan-cpan-maturity:     released

--- lib/RT/Extension/Gravatar.pm
+++ lib/RT/Extension/Gravatar.pm
@@ -4,9 +4,8 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '1.00';
 
-require RT::User;
 package RT::User;
 
 use Digest::MD5 qw(md5_hex);
@@ -14,77 +13,101 @@ use LWP::UserAgent;
 
 sub GravatarUrl {
     my $self = shift;
-    my $email = $self->EmailAddress;
-    return undef unless defined $email && length $email;
+    my $size = shift;
+
+    my $email = $self->EmailAddress || '';
+    return unless length $email;
     
-    my $gravatar_base_url;
-    if ( defined $ENV{HTTPS} and $ENV{'HTTPS'} eq 'on' ) {
-        $gravatar_base_url = 'https://secure.gravatar.com/avatar/';
-    } else {
-        $gravatar_base_url = 'http://gravatar.com/avatar/';
-    }
+    my $url = ($ENV{'HTTPS'}||'') eq 'on'
+        ? 'https://secure.gravatar.com/avatar/'
+        : 'http://gravatar.com/avatar/';
     
-    return $gravatar_base_url . md5_hex( lc $email );
+    $url .= md5_hex(lc $email);
+    $url .= "?s=" . $size
+        if $size;
+
+    return $url;
 }
 
 sub HasGravatar {
     my $self = shift;
+
+    my $url = $self->GravatarUrl;
+    return 0 unless $url;
+
+    $url .= "?d=404";
+
     my $ua = LWP::UserAgent->new;
-    my $url = $self->GravatarUrl();
-    $url .= "?default=404";
-    my $response = $ua->get( $url );
+    my $response = $ua->get($url);
     
-    return ! $response->is_error( 404 );
+    return $response->is_success;
 }
-
-1;
-
-__END__
 
 =head1 NAME
 
 RT::Extension::Gravatar - Adds gravatar images to rt
 
-=head1 SYNOPSIS
+=head1 DESCRIPTION
 
-This Plugin adds an gravatar image to the following pages:
+This Plugin adds an gravatar image to the following places:
 
-	- more about ...
-	- modify user
-	- preferences => about me
+=over
 
-=head1 FUNCTIONS
+=item More about the requestors widget
 
-=over 2
+=item Modify user page
 
-=item GravatarUrl
+=item About me (Preferences)
 
-Return the gravatar image url of the user
-
-=item HasGravatar
-
-Return true if the user has an gravatar image
+=item User Summary
 
 =back
 
 =head1 INSTALLATION
 
-Installation Instructions for RT-Extension-Gravatar:
+=over
 
-	1. perl Makefile.PL
-	2. make
-	3. make install
-	4. Add 'RT::Extension::Gravatar' to @Plugins in /opt/rt3/etc/RT_SiteConfig.pm
-	5. Clear mason cache: rm -rf /opt/rt3/var/mason_data/obj
-	6. Restart webserver
+=item perl Makefile.PL
+
+=item make
+
+=item make install
+
+=item Edit your /opt/rt4/etc/RT_SiteConfig.pm
+
+Add this line:
+
+    Set(@Plugins, qw(RT::Extension::Gravatar));
+
+or add C<RT::Extension::Gravatar> to your existing C<@Plugins> line.
+
+=item Clear your mason cache
+
+    rm -rf /opt/rt4/var/mason_data/obj/*
+
+=item Restart your webserver
+
+=back
+
+=head1 METHODS ADDED TO OTHER CLASSES
+
+=head2 RT::User
+
+=head3 GravatarUrl
+
+Return the gravatar image url of the user.
+
+=head3 HasGravatar
+
+Return true if the user has an gravatar image.
 
 =head1 AUTHOR
 
 Christian Loos <cloos@netsandbox.de>
 
-=head1 COPYRIGHT AND LICENCE
+=head1 LICENCE AND COPYRIGHT
  
-Copyright (C) 2010-2011, Christian Loos.
+Copyright (C) 2010-2013, Christian Loos.
  
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
@@ -96,3 +119,5 @@ L<http://bestpractical.com/rt/>
 L<http://gravatar.com/>
 
 =cut
+
+1;
